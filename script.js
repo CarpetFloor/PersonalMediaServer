fad9fb9e3883f3265f672ad71da171a61de57f83
CarpetFloor <dylan.bartness@gmail.com>
2023-07-20 02:24:18 -0500

    nav styling

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 let socket;
+let videoOpened = false;
+let navOpen = false;
+let navRef;
 
 window.onload = function() {
     socket = io();
+
+    document.getElementById("navToggle").addEventListener("click", toggleNavMenu);
+    navRef = document.getElementById("nav");
+}
+
+function toggleNavMenu() {
+    videoOpened = true;
+    document.getElementById("video").style.display = "block";
+
+    navOpen = !(navOpen);
+
+    if(navOpen) {
+        navRef.style.display = "flex";
+    }
+    else {
+        navRef.style.display = "none";
+    }
 }
