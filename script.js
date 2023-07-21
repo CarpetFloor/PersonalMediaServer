4e62b26ee8e04ddcfb79deed8eedeae6d9566da8
CarpetFloor <dylan.bartness@gmail.com>
2023-07-21 09:34:17 -0500

    remove old code

--- script.js
+++ script.js
@@ -14,67 +14,6 @@ socket.on("sendDirectory", function(receivingDirectory) {
     document.getElementById("navToggle").addEventListener("click", toggleNavMenu);
 })
 
-function findEndIndex(startIndex, currentLevel) {
-    for(let i = startIndex; i < directory.length; i++) {
-        let splitted = directory[i].split("/");
-
-        if((splitted.length > currentLevel)) {
-            return i;
-        }
-    }
-}
-
-function setupDirectoryOriginal() {
-    // starts at 2 because Media has 2 slashes: ./Media/, meaning that there are 3 parts when split by "/"
-    let currentLevel = 3;
-    let elem = navRef;
-    let startIndex = 0;
-    let endIndex = -1;
-    
-    endIndex = findEndIndex(startIndex, currentLevel);
-        
-    for(let i = startIndex; i < endIndex; i++) {
-        let item = document.createElement("p");
-
-        let splitted = directory[i].split("/");
-        item.innerText = splitted[splitted.length - 1];
-
-        elem.appendChild(item);
-    }
-
-    let foldersCreated = [];
-
-    while(endIndex < (directory.length - 1)) {
-        ++currentLevel;
-        startIndex = endIndex;
-        endIndex = findEndIndex(startIndex, currentLevel);
-        
-        for(let i = startIndex; i < (endIndex + 1); i++) {
-            let folderSplitted = directory[i].split("/");
-            let folderName = folderSplitted[currentLevel - 2];
-            console.log("CURRENT_LEVEL ", currentLevel);
-            console.log("DIRECTORY[I] ", directory[i]);
-            console.log("FOLDER_NAME ", folderName);
-            console.log("--------------------");
-
-            if(!(foldersCreated.includes(folderName))) {
-                foldersCreated.push(folderName);
-
-                let parent;
-                if(currentLevel == 4) {
-                    addDiv(elem, folderName, currentLevel);
-                }
-                else {
-                    parent = findParent(folderSplitted);
-                    
-                    addDiv(parent, folderName, currentLevel);
-                }
-            }
-        }
-
-    }
-}
-
 function setupDirectory() {
     let foldersCreated = [];
 
@@ -104,38 +43,8 @@ function setupDirectory() {
     }
 }
 
-function findParent(splitted) {
-    let folderName = splitted[splitted.length - 3];
-
-    let element = navRef;
-    let count = 0;
-    
-    /**This function should find something, and so one iteration of the while loop should cause a return.
-     * But just in case something goes wrong, don't get stuck in an infinte loop.
-     */
-    while(count < 50) {
-        ++count;
-
-        let children = element.childNodes;
-
-        for(let i = 0; i < children.length; i++) {
-            if(children[i].nodeName == "DIV") {
-                // p element of div that represents the folder name
-                if(children[i].children[0].innerText == folderName) {
-                    return children[i];
-                }
-                else if(splitted.includes(children[i].children[0].innerText)) {
-                    element = children[i];
-    
-                    break;
-                }
-    
-            }
-        }
-    }
-}
-
 const INDENT_SIZE = 20;
+
 function addDiv(parent, name, currentLevel) {
     let div = document.createElement("div");
     div.id = name;
