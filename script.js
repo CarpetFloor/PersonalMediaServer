1eeb3c682b249f5b32b7f1d745fbcb2047f9ccb0
CarpetFloor <dylan.bartness@gmail.com>
2023-07-21 01:21:56 -0500

    client-side directory folders concept

--- script.js
+++ script.js
@@ -15,26 +15,117 @@ socket.on("sendDirectory", function(receivingDirectory) {
 })
 
 function findEndIndex(startIndex, currentLevel) {
-    for(let i = startIndex; i < directory.length - 1; i++) {
+    for(let i = startIndex; i < directory.length; i++) {
         let splitted = directory[i].split("/");
 
-        if(splitted.length > currentLevel) {
+        if((splitted.length > currentLevel) || (i == directory.length - 1)) {
             return i;
         }
     }
 }
 
 function setupDirectory() {
-    // starts at 2 because Media has 2 slashes: ./Media/
-    let currentLevel = 2;
+    // starts at 2 because Media has 2 slashes: ./Media/, meaning that there are 3 parts when split by "/"
+    let currentLevel = 3;
     let elem = navRef;
     let startIndex = 0;
-    let endIndex = findEndIndex(startIndex, currentLevel);
+    let endIndex = -1;
+    
+    endIndex = findEndIndex(startIndex, currentLevel);
+        
+    for(let i = startIndex; i < endIndex; i++) {
+        let item = document.createElement("p");
+
+        let splitted = directory[i].split("/");
+        item.innerText = splitted[splitted.length - 1];
+
+        elem.appendChild(item);
+    }
+
+    let foldersCreated = [];
+
+    while(endIndex < (directory.length - 1)) {
+        ++currentLevel;
+        startIndex = endIndex;
+        endIndex = findEndIndex(startIndex, currentLevel);
+        console.log(currentLevel, endIndex);
+        
+        for(let i = startIndex; i < (endIndex + 1); i++) {
+            let folderSplitted = directory[i].split("/");
+            let folderName = folderSplitted[currentLevel - 2];
+
+            console.log(folderSplitted);
+
+            if(!(foldersCreated.includes(folderName))) {
+                foldersCreated.push(folderName);
+
+                let parent = findParent(folderSplitted);
+
+                addDiv(parent, folderName);
+
+                let temp = elem.childNodes;
+                addFileToDiv(temp[temp.length - 1], folderSplitted[currentLevel - 1], currentLevel);
+            }
+        }
+
+    }
+
+    console.log("second part");
+}
+
+function findParent(splitted) {
+    let folderName = splitted[splitted.length - 2];
+
+    let element = navRef;
+    let count = 0;
+    
+    while(count < 10) {
+        ++count;
+        let children = element.childNodes;
+    
+        for(let i = 0; i < children.length; i++) {
+            if(children[i].nodeName == "DIV") {
+                // p element of div that represents the folder name
+                if(children[i].children[0].innerText == folderName) {
+                    return children[i];
+                }
+                else if(splitted.includes(children[i].children[0].innerText)) {
+                    element = children[i];
+    
+                    break;
+                }
+    
+            }
+        }
+    }
+}
+
+function addDiv(parent, name) {
+    let div = document.createElement("div");
+    div.style.display = "flex";
+    div.style.flexDirection = "column";
+    
+    let title = document.createElement("p");
+    title.innerText = name;
+    title.style.fontWeight = "bold";
+    title.addEventListener("click", function(){toggleFolder(this.parentNode)});
+    
+    div.appendChild(title);
+
+    parent.appendChild(div);
+}
+
+function addFileToDiv(div, name, currentLevel) {
+    let file = document.createElement("p");
+    file.innerText = name;
+    file.style.textIndent = ((currentLevel - 1) * 10) + "px";
+    file.style.display = "none";
+
+    div.appendChild(file);
 }
 
 function toggleNavMenu() {
-    videoOpened = true;
-    document.getElementById("video").style.display = "block";
+    // document.getElementById("video").style.display = "block";
 
     navOpen = !(navOpen);
 
@@ -43,5 +134,24 @@ function toggleNavMenu() {
     }
     else {
         navRef.style.display = "none";
+    }
+}
+
+function toggleFolder(elem) {
+    let children = elem.childNodes;
+    let last = children[children.length - 1];
+
+    let test = window.getComputedStyle(last).display;
+    let update = "-1";
+
+    if(test == "none") {
+        update = "block";
+    }
+    else {
+        update = "none";
+    }
+
+    for(let i = 1; i < children.length; i++) {
+        children[i].style.display = update;
     }
 }
